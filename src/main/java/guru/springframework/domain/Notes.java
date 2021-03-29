7f6a6c437e0aa4c5ef8ddbcc965c25059dfee553
EREZ-LEVI ZARINI <ZEREZ@il.ibm.com>
2021-03-29 09:30:15 +0300

    Assigment finish

---
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -0,0 +1,44 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob //JPA will store it in a CLOB field.
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+
+
+}
+//cascade operation if you delete recipe it will delete the notes as well. we don't want that now.
